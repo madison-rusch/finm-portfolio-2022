ec3368e87eb3dd7b00ab531ff21d7f2c5444d737
Madison Rusch <55900572+madison-rusch@users.noreply.github.com>
2022-09-29 15:26:03 -0500

    More methods

--- homework/PythonCheatSheet.py
+++ homework/PythonCheatSheet.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 
 ################# Data ingestion #################
+# read_excel(path): reads in excel file at path
+# sheet_name='something' lets you import the given sheet (this is an optional argument)
 df = pd.read_excel('path_to_excel_workbook', sheet_name='sheet_name')
 
 ################# Statistics Calculations #################
@@ -27,6 +29,36 @@ df.nsmallest(1, 'Column_Name')
 # corr(): calculates the correlation matrix
 df.corr()
 
+# unstack(): pivots columns and groups them as secondary rows by original row
+#    ex:       A   B   C
+#          A   1   2   3   
+#          B   4   5   6
+#          C   7   8   9
+#    becomes:
+#          A   A   1
+#              B   2   
+#              C   3
+#          B   A   4
+#              B   5   
+#              C   6
+#          C   A   7
+#              B   8   
+#              C   9
+df.unstack()
+
+# cov(): returns matrix with covariance between columns
+df.cov()
+
+# np.mat(): converts input to a matrix
+matrix_1 = np.mat([[1,2], [3,4]])
+matrix_2 = np.mat([[1,2], [7,8]])
+
+# np.matmul(A,B): matrix multiplication (equivalent to @ symbol)
+result = np.matmul(matrix_1, matrix_2)
+
+# @ symbol: used after Python v3.5 for Matrix multiplication (equivalent to np.matmul(A, B))
+# --NOTE-- this is also used for property decorators (like in Seb's class)
+result = matrix_1 @ matrix_2
 
 ################# Mathematics #################
 # Square Root
